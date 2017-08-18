97029c2c3e476ea96d5aa1db1b46c53db3ea0c5a
shumengjun <mshu@ucmerced.edu>
2017-08-18 09:54:18 -0700

    add files

---
+++ 2017_Yosemite_ script.R
@@ -0,0 +1,67 @@
+## Mengjun Shu 
+## 2017 Yosemite workshop
+
+## download.file("https://ndownloader.figshare.com/files/2292169",
+##            "data/portal_data_joined.csv")
+
+
+surveys <- read.csv("data/portal_data_joined.csv")
+
+head(surveys)
+tail(surveys)
+tail(surveys, 12)
+str(surveys)
+summary(surveys)
+dim(surveys)
+nrow(surveys)
+ncol(surveys)
+
+names(surveys)
+
+head(surveys$weight)
+tail(surveys$weight)
+str(surveys$weight)
+summary(surveys$weight)
+
+
+plot(surveys$year, surveys$weight)
+plot(surveys$hindfoot_length, surveys$weight)
+
+
+summary(surveys$month)
+hist(surveys$month)
+
+
+summary(surveys$taxa)
+levels(surveys$taxa)
+nlevels(surveys$taxa)
+hist(survey)
+
+class(surveys$taxa)
+table(surveys$taxa)
+
+## subset in base R ----
+## [rows, colums]
+
+##return all the columns
+surveys[surveys$genus == 'Ammodramus', ]
+
+
+##return part the columns
+
+surveys[surveys$genus == 'Ammodramus', c('record_id', 'month', 'weight')]
+
+#Different ways to figure out how many month is January or February
+nrow(surveys[surveys$month < 3, ]) 
+
+surveys[surveys$month == 1, ]
+
+table(surveys$month < 3)
+
+month_new <- surveys[surveys$month < 3, ]
+
+length(which(surveys$month < 3))
+
+
+
+
